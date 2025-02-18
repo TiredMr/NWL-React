fdbea82ca556fc5349987dc6c59799b44cc83d1d
TiredMr <ffabricio047@gmail.com>
2025-02-18 20:33:01 -0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import { ArrowRight, Copy, Mail } from 'lucide-react';
+import { Button } from "./componentes/button";
+import { IconButton } from './componentes/icon-buttom';
+import { InputField, InputIcon, InputRoot } from './componentes/input';
+
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button type='submit'>
+        Enviar
+        <ArrowRight/>
+      </Button>
+
+      <IconButton>
+         <Copy/>
+      </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className='size-5'/>
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   );
 }
