2f5b9b1963e45b56a17cfaa25e7cb3fe473f1841
TiredMr <ffabricio047@gmail.com>
2025-02-20 15:56:32 -0300

    Update

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,19 +1,25 @@
+'use client'
+
 import { Copy, Link } from 'lucide-react'
 import { IconButton } from '../componentes/icon-buttom'
 import { InputField, InputIcon, InputRoot } from '../componentes/input'
 
-export function InviteLinkInput() {
+interface InviteLinkInputProps {
+  inviteLink: string
+}
+
+export function InviteLinkInput({ inviteLink }: InviteLinkInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+  }
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
-      <InputField
-        readOnly
-        defaultValue="http://localhost:3000/invite/defalt1"
-      />
+      <InputField readOnly defaultValue={inviteLink} />
 
-      <IconButton className="-mr-2">
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
